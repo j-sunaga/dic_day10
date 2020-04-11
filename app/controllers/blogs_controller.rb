c78503a3beab0278a3303ac36099b31ed0d0c68b
shounen070505 <kobayashitadao@gmail.com>
2020-04-11 11:32:44 +0900

    blogsコントローラにアクションを追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,52 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: %i(show edit update destroy)
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = current_user.blogss.build(blog_params)
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを投稿しました"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+  end
+  
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました"
+  end
+
+  private
+  def set_blog
+    @blog = current_user.Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
