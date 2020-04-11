9ad704713eb943c204c75756f4d328b750f2fef5
j-sunaga <124577blue@gmail.com>
2020-04-11 11:32:03 +0900

    user登録が完了した際にuser情報の画面に遷移するように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user= User.new(user_params)
     if @user.save
-      redirect_to blogs_path, notice: "会員登録が完了しました。"
+      redirect_to user_path(@user.id), notice: "会員登録が完了しました。"
     else
       render 'new'
     end
